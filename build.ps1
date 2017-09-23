24317ce67490a97fe1323c362f164de17bb278f8
Friedrich von Never <friedrich@fornever.me>
2017-09-23 17:27:18 +0700

    Update build files for current environment

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@ Set-Location src
 .\build.cmd
 Set-Location ..
 get-childitem -path ".\kestrel" -recurse | copy-item -destination ".\artifacts"
-docker build -t hcup/odin .
+docker build -t hcup/fornever .
 # docker run -it --rm -p 80:80 hcup/odin
 # docker login stor.highloadcup.ru
-docker tag hcup/odin stor.highloadcup.ru/travels/safe_pike
-docker push stor.highloadcup.ru/travels/safe_pike
+docker tag hcup/fornever stor.highloadcup.ru/travels/nice_toucan
+docker push stor.highloadcup.ru/travels/nice_toucan
